c020e687e821ba71b77a5f6c14b58cf9d2959987
Dalyome <dalyo1190@hotmail.be>
2016-10-20 11:42:49 +0200

    Mike : Twig Final

--- src/Mike/TwigBundle/Controller/DefaultController.php
+++ src/Mike/TwigBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('MikeTwigBundle:Default:index.html.twig');
     }
+    public function conditionAction()
+    {
+        return $this->render('MikeTwigBundle:Default:condition.html.twig');
+    }
+    public function boucleAction($chiffre,$chiffre2)
+    {
+        // pour passer nos variables get au template, on doit utiliser un tableau associatif
+        return $this->render('MikeTwigBundle:Default:boucle.html.twig',
+                array("nb1"=>$chiffre,"nb2"=>$chiffre2));
+    }
 }
